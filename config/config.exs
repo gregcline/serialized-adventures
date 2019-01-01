81a16493b966eb9af0aecc467e6486aea563f81d
Greg Cline <gregrcline@gmail.com>
2019-01-01 14:55:57 -0500

    Check if removing intial config works

--- config/config.exs
+++ config/config.exs
@@ -29,8 +29,8 @@ config :arc,
   storage: Arc.Storage.GCS,
   bucket: "serialized-adventures"
 
-config :goth,
-  json: "config/gcs_credentials.json" |> Path.expand |> File.read!
+# config :goth,
+#   json: "config/gcs_credentials.json" |> Path.expand |> File.read!
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
