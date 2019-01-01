77d963322b60d2637a7eb90dae8a3cfd56f5afe6
Greg Cline <gregrcline@gmail.com>
2019-01-01 15:49:39 -0500

    Added db name

--- config/prod.exs
+++ config/prod.exs
@@ -22,6 +22,7 @@ config :logger, level: :info
 # Configure your database
 config :hello, Hello.Repo,
   url: System.get_env("DATABASE_URL"),
+  database: "serialized_adventures",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
